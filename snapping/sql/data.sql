18fe2f3549a972d1bfc50fab11ad4a40619f1dde
Etienne Trimaille <gustrimaille@yahoo.fr>
2023-01-11 14:15:22 +0100

    Clean SQL files

--- snapping/sql/data.sql
+++ snapping/sql/data.sql
@@ -10,7 +10,6 @@ SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
@@ -1973,4 +1972,3 @@ CREATE INDEX quartiers_montpellier_geom_geom_idx ON demo_snapping.quartiers_mont
 --
 -- PostgreSQL database dump complete
 --
-
