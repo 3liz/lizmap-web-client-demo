0726c2827e0c0b5884745661946076648ff75514
Etienne Trimaille <gustrimaille@yahoo.fr>
2023-07-06 11:49:41 +0200

    Add the PgRouting script for loading the panel on loading

---
+++ projects/pgrouting/media/js/pgrouting/open_pgrouting.js
@@ -0,0 +1,29 @@
+lizMap.events.on({
+    'uicreated': () => {
+        function waitForElm(selector) {
+            return new Promise(resolve => {
+                if (document.querySelector(selector)) {
+                    return resolve(document.querySelector(selector));
+                }
+
+                const observer = new MutationObserver(mutations => {
+                    if (document.querySelector(selector)) {
+                        resolve(document.querySelector(selector));
+                        observer.disconnect();
+                    }
+                });
+
+                observer.observe(document.body, {
+                    childList: true,
+                    subtree: true
+                });
+            });
+        }
+
+        waitForElm('#button-pgrouting').then((elm) => {
+            elm.click();
+            $('#button-switcher').hide();
+            $('#button-permaLink').hide();
+        });
+    }
+});
